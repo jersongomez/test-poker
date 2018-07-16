07242923ad078ba8680528ae9308dd88d249372a
maxinuss <maxinuss@gmail.com>
2018-07-16 16:50:06 -0300

    Update PokerTest.php
    Fix space

--- tests/UnitTest/PokerTest.php
+++ tests/UnitTest/PokerTest.php
@@ -16,7 +16,7 @@ class PokerTest extends TestCase
         $this->assertEquals($results2,true,"14, 5, 4, 2, 3");
 
         $results3 = $poker->isStraight([7, 7, 12, 11, 3, 4, 14]);
-        $this->assertEquals($results3,false,"7, 7, 12 ,11, 3, 4,	 14");
+        $this->assertEquals($results3,false,"7, 7, 12 ,11, 3, 4, 14");
 
         $results4 = $poker->isStraight([7, 3, 2]);
         $this->assertEquals($results4,false,"7, 3, 2");
